f7b3c517994640925b034d7b69887c2cddf85a3e
Hack Reactor Students <tyler.a.ochiai@gmail.com>
2014-06-13 16:40:50 -0700

    Add enemies to dom as g elements

--- watchout.js
+++ watchout.js
@@ -1,6 +1,6 @@
 (function watchout(){
 
-  var svg = d3.selectAll("body").append("svg")
+  var svg = d3.select("body").append("svg")
     .attr("width", 700)
     .attr("height", 450)
     .append("g");
@@ -10,7 +10,17 @@
 // encapsulate draw enemies into a function
   //takes random start positions as first data
   //
-
+  var drawEnemies = function(numEnemies) {
+    var positions = [];
+    for(var i = 0; i < numEnemies; i++) {
+      positions.push({x: Math.random() * 700, y: Math.random() * 450});
+    }
+    d3.selectAll("g").data(positions).enter().append("g")
+      .attr("x", function(d){ return d.x;})
+      .attr("y", function(d) { return d.y;})
+      .attr("class", "enemy");
+  };
+drawEnemies(5);
 // encapsulate update enemies location into a function
   // takes random values as parameters
   //  use transition and maybe duration functions to move
