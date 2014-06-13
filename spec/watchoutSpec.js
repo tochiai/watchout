56a85a908abac255578ca85250aa3dd4de4d6782
Hack Reactor Students <tyler.a.ochiai@gmail.com>
2014-06-13 16:27:57 -0700

    Pseudocode and create svg element

--- spec/watchoutSpec.js
+++ spec/watchoutSpec.js
@@ -1,56 +1,11 @@
-describe('solvers', function() {
-  window.displayBoard = function() {};
+describe('watchout', function() {
 
-  describe('findNRooksSolution()', function() {
+  describe('creates svg', function() {
 
-    it('finds a valid solution for n of 1-8', function() {
-      _.range(1, 9).map(function(n) {
-        var solutionBoard = new Board(findNRooksSolution(n));
-
-        expect(solutionBoard.get('n')).to.equal(n);
-        expect(solutionBoard.hasAnyRooksConflicts()).to.be.equal(false);
-      });
+    it('should have one svg element', function() {
+      var numSvg = d3.selectAll("svg")[0].length
+        expect(numSvg).to.be.equal(1);
     });
 
   });
-
-  describe('countNRooksSolutions()', function() {
-
-    it('finds the number of valid solutions for n of 1-8', function() {
-      _.range(1, 9).map(function(n) {
-        var solutionCount = countNRooksSolutions(n);
-        var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n];
-
-        expect(solutionCount).to.be.equal(expectedSolutionCount);
-      });
-    });
-
-  });
-
-  describe('findNQueensSolution()', function() {
-
-    it('finds a valid solution for n of 0-8', function() {
-      _.range(1, 8).map(function(n) {
-        var solutionBoard = new Board(findNQueensSolution(n));
-
-        expect(solutionBoard.get('n')).to.equal(n);
-        expect(solutionBoard.hasAnyQueensConflicts()).to.be.equal(false);
-      });
-    });
-
-  });
-
-  describe('countNQueensSolutions()', function() {
-
-    it('finds the number of valid solutions for n of 0-8', function() {
-      _.range(0, 9).map(function(n) {
-        var solutionCount = countNQueensSolutions(n);
-        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
-
-        expect(solutionCount).to.be.equal(expectedSolutionCount);
-      });
-    });
-
-  });
-
 });
--- watchout.js
+++ watchout.js
@@ -1,2 +1,25 @@
-// start slingin' some d3 here.
+(function watchout(){
 
+  var svg = d3.selectAll("body").append("svg")
+    .attr("width", 700)
+    .attr("height", 450)
+    .append("g");
+
+
+
+// encapsulate draw enemies into a function
+  //takes random start positions as first data
+  //
+
+// encapsulate update enemies location into a function
+  // takes random values as parameters
+  //  use transition and maybe duration functions to move
+
+// encapsulate player create into a function
+  // give player some kind of 'on() do()' function
+
+// collision detection
+
+// keep track of user score
+
+})();
