ba31cb5468089a8b49234618c31036ff3454208c
Hack Reactor Students <tyler.a.ochiai@gmail.com>
2014-06-13 17:18:43 -0700

    Animates enemies

--- watchout.js
+++ watchout.js
@@ -1,5 +1,5 @@
 (function watchout(){
-
+  var numEnemies = 5;
   var svg = d3.select("body").append("svg")
     .attr("width", 700)
     .attr("height", 450);
@@ -12,7 +12,7 @@
   var drawEnemies = function(numEnemies) {
     var positions = [];
     for(var i = 0; i < numEnemies; i++) {
-      positions.push({x: Math.random() * 700, y: Math.random() * 450});
+      positions.push({x: Math.random() * 675, y: Math.random() * 425});
     }
     d3.select("svg").selectAll("circle").data(positions).enter().append("circle")
       .attr("cx", function(d){ return d.x;})
@@ -20,7 +20,21 @@
       .attr("r", 10)
       .attr("class", "enemy");
   };
-drawEnemies(5);
+  drawEnemies(numEnemies);
+
+  var moveEnemies = function(){
+    var newPositions = [];
+    for(var i = 0; i < numEnemies; i++) {
+      newPositions.push({x: Math.random() * 700, y: Math.random() * 450});
+    }
+    d3.selectAll(".enemy").data(newPositions)
+      .transition()
+      .duration(200)
+      .attr("cx", function(d){ return d.x;})
+      .attr("cy", function(d) { return d.y;})
+  };
+
+  setInterval(moveEnemies, 1000);
 // encapsulate update enemies location into a function
   // takes random values as parameters
   //  use transition and maybe duration functions to move
